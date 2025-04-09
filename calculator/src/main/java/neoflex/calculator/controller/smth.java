ce73e94b1fe5684a2e5af7260591adfeaa5e4b10
Mur0riki <nevzlomaite2283@mail.ru>
2025-04-09 17:39:31 +0300

    add something

---
+++ calculator/src/main/java/neoflex/calculator/controller/smth.java
@@ -0,0 +1,11 @@
+package neoflex.calculator.controller;
+
+public class smth {
+    private final String xd;
+
+    public smth(String xd) {
+        this.xd = xd;
+    }
+    private int zv;
+    private int zx;
+}
